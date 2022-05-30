9a5f9567601ad69085b0de70e310662d15b9952e
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-30 13:07:06 +0530

    reformat the file and write main function to run the module

--- faceDetectionModule.py
+++ faceDetectionModule.py
@@ -1,8 +1,13 @@
 import cv2
 
 
-img = cv2.imread('Resources/elonMusk.jpg')
-img = cv2.resize(img, (740, 480))
+def main():
+    img = cv2.imread('Resources/elonMusk.jpg')
+    img = cv2.resize(img, (740, 480))
 
-cv2.imshow('Img', img)
-cv2.waitKey(0)
+    cv2.imshow('Img', img)
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    main()
